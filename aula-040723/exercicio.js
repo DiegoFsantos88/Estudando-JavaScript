dd854711503b804cd9b5188c646c9185e5d379cc
DiegoFSantos88 <diegofsccp@gmail.com>
2023-07-04 13:57:56 -0300

    exerciciojs

---
+++ aula-040723/exercicio.js
@@ -0,0 +1,55 @@
+// let num1 = 2122  //verificar par ou impar
+// let mensagem
+// let condicao = (num1 % 2 == 0)
+// mensagem = condicao? "Par" : "impar"
+// console.log(mensagem)
+
+
+// let numero1 = 5; // média aritimética
+// let numero2 = 8;
+// let numero3 = 12;
+// let media = (numero1 + numero2 + numero3) / 3; 
+
+// console.log("A média é: " + media);
+
+// // Função para verificar se um número é divisível por 3 e por 5
+// function verificarDivisivelPor3e5(numero) {
+//     if (numero % 3 === 0 && numero % 5 === 0) {
+//     return 'O número é divisível por 3 e por 5.';
+//     } else {
+//     return 'O número não é divisível por 3 e por 5.';
+//     }
+//   }
+//     let numero = 15;
+//   let resultado = verificarDivisivelPor3e5(250);
+//   console.log(resultado); 
+  
+//Faça um programa que calcule a área de um triângulo com base e altura.
+
+// let base = 50;
+// let altura = 2;
+// let area = (base * altura) / 2;
+// console.log("A area do triângulo é:" + area)
+
+
+//Escreva um programa que converta uma temperatura de graus Celsius para Fahrenheit.
+
+// let celsius = 45;
+// let fahrenheit = celsius * 1.8 + 32;
+
+// console.log(celsius + "graus celsius são iguais a" + fahrenheit + "graus fahrenheit");
+
+//Faça um programa que calcule o delta das funções de segundo grau.
+
+// let a = 4;
+// let b = 16;
+// let c = -48;
+// let delta = b ** 2 - 4 * a * c;
+
+// console.log("O delta da equação é igual a:" + delta);
+
+//Faça um programa que retorna se uma determinada pessoa pode dirigir, baseado na sua idade (utilize operador ternário).
+
+let idade = 17;
+let mensagem = (idade >=18)? "Pode tirar carteira de motorista" : "Não pode tirar carteira de motorista"
+console.log(mensagem);
