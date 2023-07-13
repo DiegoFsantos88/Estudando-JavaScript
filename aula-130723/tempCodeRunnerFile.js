cb9f630206a6d88bb4195362b9e69aafcc22af56
DiegoFSantos88 <diegofsccp@gmail.com>
2023-07-13 13:05:38 -0300

    vetores

---
+++ aula-130723/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+let nota = 9.6;
+// let notas = [6.5, 7.8, 9.0]; //variavel composta
+// let times = ["Botafogo", "Corinthians", "Gremio"]
+// let ativados = [true, false, false, true];
+// let daniel = ["Daniel", 23, "Ensino Superior", true]; //uma má prática
+// console.log(typeof nota); //number
+// console.log(typeof notas);//object
+// console.log(notas.length);//tamanho do vetor
+
+
+// let pesos = [89.5, 60.0, 45.4, 67.0, 75.3];
+// console.log(pesos.length);
+// console.log(pesos[-1]); //indice -> numero que representa a posicao do elemento começa de "0"
+
+// for(let i = 0; i < pesos.length; i++) {
+//     console.log(pesos[i]);
+//     }
